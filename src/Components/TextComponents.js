c58e6c2682120f406b9db971bfdbc37be17c9e01
Kenneth Lai <knthslai@gmail.com>
2018-09-30 18:40:45 -0400

    missing punctuation

--- src/Components/TextComponents.js
+++ src/Components/TextComponents.js
@@ -6,7 +6,7 @@ module.exports = {
   aboutMe: [{ header: `Where did it all start?`, text: `After pitching and acquiring the position of a database administrator for a startup, I experienced the true nature of being a developer. Starting from the ground up, I had to research and implement all relevant tasks from handling database requests to designing front end clients. This allowed my problems to become opportunities and my work to become a passion.`, img: [`benesse.jpg`] },
   { header: `Hello world!`, text: `There were no doubts in my mind to make this passion my career, which was why I attended Fullstack Academy immersive dev program in NYC. It was there that I obtained the best practices and relevant tools to compete with current developers on the field.`, img: [`FSA.JPG`] },
   {
-    header: `Only up from here`, text: `I have found my love for programming very naturally when serving up database queries. As well as anything in between that and launching projects to a functional state. I currently specialize in full stack development with JavaScript integrations here in NYC.`, img: [`Future.gif`]
+    header: `Only up from here!`, text: `I have found my love for programming very naturally when serving up database queries. As well as anything in between that and launching projects to a functional state. I currently specialize in full stack development with JavaScript integrations here in NYC.`, img: [`Future.gif`]
   }
   ],
   projects: [{
